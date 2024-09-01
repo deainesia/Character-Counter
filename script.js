40acadbca80c4e0ba51fb9469f0d3c685fc81074
Dea Inesia Sri Utami <deainesia223@gmail.com>
2024-09-01 15:14:29 +0700

    Logic for frequent word undone. The pattern count has success.

--- script.js
+++ script.js
@@ -7,7 +7,8 @@ var counterWord = document.getElementById("total-word"); //total word text handl
 var maxCharacter = document.getElementById("max-text"); //maximum character text handle
 var remainCharacter = document.getElementById("remain-text"); //remaining character text handle
 var textArray;
-
+var textArrayLength;
+var buttonFrequent = document.getElementById("check-freq"); //check frequent word button handle
 
 function checkRemainCharacter(maxValue) { //function for check the remain character by maximum character
   if (maxValue - text.value.length >= 0) { //if character have not reached the maximum
@@ -61,9 +62,27 @@ text.addEventListener("input", (event) => { //add listener textarea handle
     checkRemainCharacter(optionMax);
   }
   //word counter
-  textArray = text.value.split(" ").filter((str) => { //split and filter text by whitespace
+  textArrayLength = text.value.split(" ").filter((str) => { //split and filter text by whitespace
     return /\S/.test(str); // return no whitespace in array
   }).length; //length of array
 
-  counterWord.textContent = textArray;
+  counterWord.textContent = textArrayLength;
+  // return textArray;
 }) 
+
+buttonFrequent.addEventListener("click", (event) => {
+  const freqWord = new Array();
+  textArray = text.value.split(/[\s.,&!":;?]/).filter((str) => { //split and filter text
+    return /\S/.test(str); // return no whitespace in array
+  });
+  textArray.forEach(i => {
+    freqWord[i] = (freqWord[i] || 0) + 1; //short circuiting logic -> if freqWord[i] is empty then 0 will be picked. Then + the value with 1.
+    // console.log(freqWord);
+    // console.log(typeof(freqWord));
+    
+  });
+
+  // what if we turn textarray to map. so initial map has a name refer to the word and it has freq refer to amount of word found.
+  // then fill the map with help of foreach. idk man. come back tmr
+
+})
